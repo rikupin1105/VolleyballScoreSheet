d54bc4dc10adb5b26de1e8a7d44379619207e2f0
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-16 19:42:09 +0900

    Create RedCard.xaml.cs

---
+++ VolleyballScoreSheet/Views/Card/RedCard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VolleyballScoreSheet.Views.Card
+{
+    /// <summary>
+    /// Interaction logic for RedCard
+    /// </summary>
+    public partial class RedCard : UserControl
+    {
+        public RedCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
