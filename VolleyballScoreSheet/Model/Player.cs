14932786c4c13b7c6f0f51291e8f515ca13f861b
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-17 23:22:23 +0900

    Update Player.cs

--- VolleyballScoreSheet/Model/Player.cs
+++ VolleyballScoreSheet/Model/Player.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VolleyballScoreSheet.Model
 {
-    public class Player
+    public class Player : BinableBase
     {
-        public int Id { get; set; }
+        public Player(int id, string? name, bool isLibero = false, bool isCaptain = false)
+        {
+            Id=id;
+            Name=name;
+            IsLibero=isLibero;
+            IsCaptain=isCaptain;
+        }
+        private int _id;
+        [Required]
+        [Range(0, 99)]
+        public int Id { get => _id; set => SetProperty(ref _id, value); }
         public string? Name { get; set; }
-        public bool IsLebero { get; set; }
+        public bool IsLibero { get; set; }
         public bool IsCaptain { get; set; }
     }
 }
