7287d8931c3e1b1adacce6616183bb1922520a27
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-19 16:52:08 +0900

    記録用紙に罰則を実装

---
+++ VolleyballScoreSheet/Views/ScoreSheet/Sanctions.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VolleyballScoreSheet.Views.ScoreSheet
+{
+    /// <summary>
+    /// Interaction logic for Sanctions
+    /// </summary>
+    public partial class Sanctions : UserControl
+    {
+        public Sanctions()
+        {
+            InitializeComponent();
+        }
+    }
+}
