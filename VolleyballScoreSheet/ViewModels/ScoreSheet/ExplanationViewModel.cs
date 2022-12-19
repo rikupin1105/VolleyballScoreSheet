dee17eaf5bf84085579f6748b7e1bad73ed9dbbe
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-20 01:49:50 +0900

    スコアシートの説明欄を追加

---
+++ VolleyballScoreSheet/ViewModels/ScoreSheet/ExplanationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VolleyballScoreSheet.ViewModels.ScoreSheet
+{
+    public class ExplanationViewModel : BindableBase
+    {
+        public ExplanationViewModel()
+        {
+
+        }
+    }
+}
