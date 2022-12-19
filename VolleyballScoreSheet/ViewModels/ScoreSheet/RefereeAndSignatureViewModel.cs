b176a3cc78178f39959937266af4946ace3c2c29
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-20 01:50:19 +0900

    審判員とサイン欄を追加

---
+++ VolleyballScoreSheet/ViewModels/ScoreSheet/RefereeAndSignatureViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VolleyballScoreSheet.Model;
+
+namespace VolleyballScoreSheet.ViewModels.ScoreSheet
+{
+    public class RefereeAndSignatureViewModel : BindableBase
+    {
+        private readonly Game _game;
+        public RefereeAndSignatureViewModel(Game game)
+        {
+            _game=game;
+            Referees = _game.Referees;
+        }
+        public Referees Referees { get; set; } = new();
+
+    }
+}
---
+++ VolleyballScoreSheet/Views/ScoreSheet/RefereeAndSignature.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VolleyballScoreSheet.Views.ScoreSheet
+{
+    /// <summary>
+    /// Interaction logic for RefereeAndSignature
+    /// </summary>
+    public partial class RefereeAndSignature : UserControl
+    {
+        public RefereeAndSignature()
+        {
+            InitializeComponent();
+        }
+    }
+}
