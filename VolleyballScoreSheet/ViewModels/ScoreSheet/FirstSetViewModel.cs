7d9d4984836a4e595416a958784789b8b94eac47
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-23 17:07:36 +0900

    FirstSetをMVVM

--- VolleyballScoreSheet/ViewModels/ScoreSheet/FirstSetViewModel.cs
+++ VolleyballScoreSheet/ViewModels/ScoreSheet/FirstSetViewModel.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Security.Cryptography.Xml;
 using System.Windows.Media;
+using VolleyballScoreSheet.Model.Scoresheet;
 
 namespace VolleyballScoreSheet.ViewModels.ScoreSheet
 {
@@ -17,311 +18,73 @@ namespace VolleyballScoreSheet.ViewModels.ScoreSheet
         public FirstSetViewModel(Game game)
         {
             _game = game;
-            Refresh();
-        }
-        public void Refresh()
-        {
-            LeftTeamName = _game.ATeam.Value.Name.Value;
-            RightTeamName = _game.BTeam.Value.Name.Value;
 
+            var firstSet = new FirstSet(game);
 
-            if (_game.CoinToss.ATeamServer)
-            {
-                LeftTeamServe = true;
-                LeftTeamReception = false;
-                LeftServeCheckList.Add(true);
-                RightServeCheckList.Add(false);
-                RightPointList.Add(null);
-            }
-            else
-            {
-                LeftTeamServe = false;
-                LeftTeamReception = true;
-                LeftServeCheckList.Add(false);
-                RightServeCheckList.Add(true);
-                LeftPointList.Add(null);
-            }
+            StartTime = firstSet.StartTime;
+            EndTime = firstSet.EndTime;
+            isEndSet = firstSet.isEndSet;
 
-            LeftTeamStartingLineUp = _game.ATeam.Value.StartingLineUp.Value;
-            RightTeamStartingLineUp = _game.BTeam.Value.StartingLineUp.Value;
+            LeftSubstitutionPoint = firstSet.LeftSubstitutionPoint;
+            RightSubstitutionPoint= firstSet.RightSubstitutionPoint;
 
-            LeftTeamSubstitutioned = _game.ATeam.Value.Substitutioned.Value;
-            RightTeamSubstitutioned = _game.BTeam.Value.Substitutioned.Value;
+            LeftFinalPoint= firstSet.LeftFinalPoint;
+            RightFinalPoint= firstSet.RightFinalPoint;
 
-            LeftTeamIsReturn = _game.ATeam.Value.isReturn.Value;
-            RightTeamIsReturn = _game.BTeam.Value.isReturn.Value;
+            LeftPointSlash = firstSet.LeftPointSlash;
+            RightPointSlash = firstSet.RightPointSlash;
 
-            var leftfrag = false;
-            var apoint = 0;
-            var bpoint = 0;
-            for (int i = 0; i < _game.History.Histories.Value.Count; i++)
-            {
-                if (_game.History.Histories.Value[i].Command1 == "S1")
-                {
-                    leftfrag = true;
+            LeftTimeouts = firstSet.LeftTimeouts;
+            RightTimeouts = firstSet.RightTimeouts;
 
-                    StartTime = _game.History.Histories.Value[i].DateTime.ToString("HH:mm");
-                }
-                if (leftfrag == true && _game.History.Histories.Value[i].Command1 == "WSA" || _game.History.Histories.Value[i].Command1 == "WSB")
-                {
-                    EndTime = _game.History.Histories.Value[i].DateTime.ToString("HH:mm");
+            LeftTeamServe = firstSet.LeftTeamServe;
+            LeftTeamReception = firstSet.LeftTeamReception;
+            LeftTeamName = firstSet.LeftTeamName;
+            RightTeamName = firstSet.RightTeamName;
 
-                    if (_game.History.Histories.Value[i].Command1 == "WSA")
-                    {
-                        for (int k = i - 2; 0 < i; k--)
-                        {
-                            if (_game.History.Histories.Value[k].Command1 == "PointA")
-                            {
-                                break;
-                            }
-                            else if (_game.History.Histories.Value[k].Command1 == "PointB")
-                            {
-                                LeftServeCheckList.RemoveAt(LeftServeCheckList.Count-1);
-                                break;
-                            }
-                        }
-                    }
-                    if (_game.History.Histories.Value[i].Command1 == "WSB")
-                    {
-                        for (int k = i - 2; 0 < i; k--)
-                        {
-                            if (_game.History.Histories.Value[k].Command1 == "PointA")
-                            {
-                                RightServeCheckList.RemoveAt(LeftServeCheckList.Count-1);
-                                break;
-                            }
-                            else if (_game.History.Histories.Value[k].Command1 == "PointB")
-                            {
-                                break;
-                            }
-                        }
-                    }
+            LeftTeamStartingLineUp = firstSet.LeftTeamStartingLineUp;
+            RightTeamStartingLineUp = firstSet.RightTeamStartingLineUp;
+            LeftTeamSubstitutioned = firstSet.LeftTeamSubstitutioned;
+            RightTeamSubstitutioned = firstSet.RightTeamSubstitutioned;
+            LeftTeamIsReturn = firstSet.LeftTeamIsReturn;
+            RightTeamIsReturn = firstSet.RightTeamIsReturn;
 
-                    isEndSet = true;
-                    //セット終了
-                    if (LeftPointList[^1] != apoint)
-                    {
-                        LeftPointList.Add(apoint);
-                    }
-                    if (RightPointList[^1] != bpoint)
-                    {
-                        RightPointList.Add(bpoint);
-                    }
+            LeftDeletePoint = firstSet.LeftDeletePoint;
+            RightDeletePoint = firstSet.RightDeletePoint;
 
+            LeftPoints = firstSet.LeftPoints;
+            RightPoints = firstSet.RightPoints;
 
-                    //閉じる
-                    LeftDeletePoint = new DeletePoint().DeletePointCulc(apoint);
-                    RightDeletePoint = new DeletePoint().DeletePointCulc(bpoint);
-
-                    //最後の位置に丸をつける
-                    LeftFinalPoint = LastPosition(LeftPointList.Count);
-                    RightFinalPoint = LastPosition(RightPointList.Count);
-
-                    break;
-                }
-                if (leftfrag)
-                {
-                    if (_game.History.Histories.Value[i].Command1 == "PointA")
-                    {
-                        apoint++;
-
-                        if (RightPointList.Count == 0)
-                        {
-                            RightPointList.Add(bpoint);
-                            LeftServeCheckList.Add(true);
-                        }
-                        else if (RightPointList[^1] == null && bpoint == 0)
-                        {
-
-                        }
-                        else if (RightPointList[^1] != bpoint)
-                        {
-                            RightPointList.Add(bpoint);
-                            LeftServeCheckList.Add(true);
-                        }
-                    }
-                    else if (_game.History.Histories.Value[i].Command1 == "PointB")
-                    {
-                        bpoint++;
-
-                        if (LeftPointList.Count == 0)
-                        {
-                            LeftPointList.Add(apoint);
-                            RightServeCheckList.Add(true);
-                        }
-                        else if (LeftPointList[^1] == null && apoint == 0)
-                        {
-
-                        }
-                        else if (LeftPointList[^1] != apoint)
-                        {
-                            LeftPointList.Add(apoint);
-                            RightServeCheckList.Add(true);
-                        }
-
-                    }
-                    else if (_game.History.Histories.Value[i].Command1 == "SubstitutionA")
-                    {
-                        var a = _game.History.Histories.Value[i].Command2.Split(',');
-
-
-                        var index = Array.IndexOf(LeftTeamStartingLineUp, int.Parse(a[0]));
-                        if (index != -1)
-                        {
-                            LeftSubstitutionPoint[index*2+1] = apoint +" : "+bpoint;
-                        }
-                        var index1 = Array.IndexOf(LeftTeamStartingLineUp, int.Parse(a[1]));
-                        if (index1 != -1)
-                        {
-                            LeftSubstitutionPoint[index1*2] = apoint +" : "+bpoint;
-                        }
-                    }
-                    else if (_game.History.Histories.Value[i].Command1 == "SubstitutionB")
-                    {
-                        var a = _game.History.Histories.Value[i].Command2.Split(',');
-
-
-                        var index = Array.IndexOf(RightTeamStartingLineUp, int.Parse(a[0]));
-                        if (index != -1)
-                        {
-                            RightSubstitutionPoint[index*2+1] = bpoint +" : "+apoint;
-                        }
-                        var index1 = Array.IndexOf(RightTeamStartingLineUp, int.Parse(a[1]));
-                        if (index1 != -1)
-                        {
-                            RightSubstitutionPoint[index1*2] = bpoint +" : "+apoint;
-                        }
-                    }
-                    else if (_game.History.Histories.Value[i].Command1 =="TimeOutA")
-                    {
-                        LeftTimeouts.Add(apoint +" : "+bpoint);
-                    }
-                    else if (_game.History.Histories.Value[i].Command1 =="TimeOutB")
-                    {
-                        RightTimeouts.Add(bpoint +" : "+apoint);
-                    }
-                }
-            }
-            for (int i = 0; i < apoint; i++)
-            {
-                LeftPointSlash[i] = true;
-            }
-            for (int i = 0; i < bpoint; i++)
-            {
-                RightPointSlash[i] = true;
-            }
-
-
-            if (!_game.CoinToss.ATeamLeftSide)
-            {
-                (LeftPointList, RightPointList) = (RightPointList, LeftPointList);
-                (LeftTimeouts, RightTimeouts) = (RightTimeouts, LeftTimeouts);
-                (LeftSubstitutionPoint, RightSubstitutionPoint) = (RightSubstitutionPoint, LeftSubstitutionPoint);
-                (LeftPointSlash,RightPointSlash)=(RightPointSlash, LeftPointSlash);
-                (LeftFinalPoint, RightFinalPoint)=(RightFinalPoint, LeftFinalPoint);
-                (LeftServeCheckList, RightServeCheckList) = (RightServeCheckList, LeftServeCheckList);
-                (LeftDeletePoint, RightDeletePoint) = (RightDeletePoint, LeftDeletePoint);
-
-                (LeftTeamName, RightTeamName) = (RightTeamName, LeftTeamName);
-                (LeftTeamStartingLineUp, RightTeamStartingLineUp) = (RightTeamStartingLineUp, LeftTeamStartingLineUp);
-                (LeftTeamSubstitutioned,RightTeamSubstitutioned) = (RightTeamSubstitutioned, LeftTeamSubstitutioned);
-                (LeftTeamIsReturn,RightTeamIsReturn) = (RightTeamIsReturn, LeftTeamIsReturn);
-
-                LeftTeamServe = !LeftTeamServe;
-                LeftTeamReception = !LeftTeamReception;
-            }
+            LeftServeCheck = firstSet.LeftServeCheck;
+            RightServeCheck = firstSet.RightServeCheck;
         }
 
-        public int[] LastPosition(int x)
-        {
-            var array = new int[2];
-            array[0] = ((int)Math.Ceiling(x / 6.0) - 1) % 4;
-            array[1] = (x % 6 - 1) * 2;
-            if (x%6==0) array[1] = 10;
-            if (x>24) array[1]+=1;
-            return array;
-        }
+        
 
-        public string StartTime { get; set; }
-        public string EndTime { get; set; }
+        public string? StartTime { get; set; }
+        public string? EndTime { get; set; }
+        public bool isEndSet { get; set; }
 
-        public string[] LeftSubstitutionPoint { get; set; } = new string[12];
-        public string[] RightSubstitutionPoint { get; set; } = new string[12];
+        public string[] LeftSubstitutionPoint { get; set; }
+        public string[] RightSubstitutionPoint { get; set; }
 
-        public bool isEndSet { get; set; } = false;
-        public int[] LeftFinalPoint { get; set; } = new int[2];
-        public int[] RightFinalPoint { get; set; } = new int[2];
+        public int[] LeftFinalPoint { get; set; }
+        public int[] RightFinalPoint { get; set; }
 
-        public bool[] LeftPointSlash { get; set; } = new bool[45];
-        public bool[] RightPointSlash { get; set; } = new bool[45];
+        public bool[] LeftPointSlash { get; set; }
+        public bool[] RightPointSlash { get; set; }
 
-        private List<bool> LeftServeCheckList { get; set; } = new();
-        private List<bool> RightServeCheckList { get; set; } = new();
-        public bool?[] LeftServeCheck
-        {
-            get
-            {
-                var array = new bool?[48];
-
-                for (int i = 0; i<LeftServeCheckList.Count; i++)
-                {
-                    array[i] = LeftServeCheckList[i];
-                }
-
-                return array;
-            }
-        }
-        public bool?[] RightServeCheck
-        {
-            get
-            {
-                var array = new bool?[48];
-
-                for (int i = 0; i<RightServeCheckList.Count; i++)
-                {
-                    array[i] = RightServeCheckList[i];
-                }
-
-                return array;
-            }
-        }
+        public bool?[] LeftServeCheck { get; set; }
+        public bool?[] RightServeCheck { get; set; }
 
         //サーブのところ
-        private List<int?> LeftPointList { get; set; } = new();
-        private List<int?> RightPointList { get; set; } = new();
-        public int?[] LeftPoints
-        {
-            get
-            {
-                var array = new int?[48];
-
-                for (int i = 0; i<LeftPointList.Count; i++)
-                {
-                    array[i] = LeftPointList[i];
-                }
-
-                return array;
-            }
-        }
-        public int?[] RightPoints
-        {
-            get
-            {
-                var array = new int?[48];
-
-                for (int i = 0; i < RightPointList.Count; i++)
-                {
-                    array[i] = RightPointList[i];
-                }
-
-                return array;
-            }
-        }
+        public int?[] LeftPoints { get; set; }
+        public int?[] RightPoints { get; set; }
 
 
         //タイムアウト
-        public List<string> LeftTimeouts { get; set; } = new();
-        public List<string> RightTimeouts { get; set; } = new();
+        public List<string> LeftTimeouts { get; set; }
+        public List<string> RightTimeouts { get; set; }
 
         public bool LeftTeamServe { get; set; }
         public bool LeftTeamReception { get; set; }
@@ -337,95 +100,9 @@ namespace VolleyballScoreSheet.ViewModels.ScoreSheet
         public bool[] LeftTeamIsReturn { get; set; } = new bool[6];
         public bool[] RightTeamIsReturn { get; set; } = new bool[6];
 
-        public DeletePoint[] LeftDeletePoint { get; set; } = new DeletePoint[5] { new DeletePoint(), new DeletePoint(), new DeletePoint(), new DeletePoint(), new DeletePoint() };
-        public DeletePoint[] RightDeletePoint { get; set; } = new DeletePoint[5] { new DeletePoint(), new DeletePoint(), new DeletePoint(), new DeletePoint(), new DeletePoint() };
+        public FirstSet.DeletePoint[] LeftDeletePoint { get; set; }
+        public FirstSet.DeletePoint[] RightDeletePoint { get; set; }
 
-        public class DeletePoint
-        {
-            public DeletePoint() { }
-            public DeletePoint[] DeletePointCulc(int point)
-            {
-                var array = new DeletePoint[5]
-                {
-                    new DeletePoint(),
-                    new DeletePoint(),
-                    new DeletePoint(),
-                    new DeletePoint(),
-                    new DeletePoint()
-                };
-
-                if (point <= 36)
-                {
-                    array[4] = new DeletePoint()
-                    {
-                        Visible = true
-                    };
-                    array[3] = new DeletePoint()
-                    {
-                        Visible = true,
-                        StartRow = point - 27
-                    };
-                }
-                if (point <= 27)
-                {
-                    array[3] = new DeletePoint()
-                    {
-                        Visible = true
-                    };
-                    array[2] = new DeletePoint()
-                    {
-                        Visible = true,
-                        StartRow = point - 18
-                    };
-                }
-                if (point <= 18)
-                {
-                    array[2] = new DeletePoint()
-                    {
-                        Visible = true
-                    };
-                    array[1] = new DeletePoint()
-                    {
-                        Visible = true,
-                        StartRow = point - 9
-                    };
-                }
-                if (point <= 9)
-                {
-                    array[1] = new DeletePoint()
-                    {
-                        Visible = true
-                    };
-
-                    array[0] = new DeletePoint()
-                    {
-                        Visible = true,
-                        StartRow = point
-                    };
-                }
-
-                return array;
-            }
-            public bool Visible { get; set; } = false;
-            public int StartRow { get; set; } = 0;
-            public int EndRow { get; set; } = 9;
-
-            public int Y
-            {
-                get
-                {
-                    if (StartRow == 0) return 1;
-                    if (StartRow == 1) return 14;
-                    if (StartRow == 2) return 28;
-                    if (StartRow == 3) return 41;
-                    if (StartRow == 4) return 54;
-                    if (StartRow == 5) return 67;
-                    if (StartRow == 6) return 80;
-                    if (StartRow == 7) return 94;
-                    if (StartRow == 8) return 107;
-                    return 0;
-                }
-            }
-        }
+        
     }
 }
