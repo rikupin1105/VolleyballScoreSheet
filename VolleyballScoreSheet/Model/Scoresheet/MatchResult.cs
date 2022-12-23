b3ce5780a19afaa783f4ab4881171c9cffdcca93
kaminaka rikuya <rikupin1105@gmail.com>
2022-12-23 13:03:58 +0900

    MatchResultをMVVMに

---
+++ VolleyballScoreSheet/Model/Scoresheet/MatchResult.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolleyballScoreSheet.Model.Scoresheet
+{
+    public class MatchResult
+    {
+        private readonly Game _game;
+        public MatchResult(Game game)
+        {
+            _game = game;
+
+            ATeamName = _game.ATeam.Value.Name.Value;
+            BTeamName = _game.BTeam.Value.Name.Value;
+
+            for (int i = 0; i < _game.Set.Value; i++)
+            {
+                ASet[i] = new()
+                {
+                    Timeouts = _game.ATeam.Value.Sets[i].TimeOuts.Value,
+                    Substitutions = _game.ATeam.Value.Sets[i].Substitutions.Value + _game.ATeam.Value.Sets[i].SubstitutionDetails.Where(x => x.ExceptionalSubstitution == true).Count(),
+
+                    Points = _game.ATeam.Value.Sets[i].Points.Value,
+
+                };
+                BSet[i] = new()
+                {
+                    Timeouts = _game.BTeam.Value.Sets[i].TimeOuts.Value,
+                    Substitutions = _game.BTeam.Value.Sets[i].Substitutions.Value + _game.BTeam.Value.Sets[i].SubstitutionDetails.Where(x => x.ExceptionalSubstitution == true).Count(),
+
+                    Points = _game.BTeam.Value.Sets[i].Points.Value,
+                };
+            }
+
+            SetDuration[0] = Duration(_game.History.Histories.Value.Where(x => x.Command1 == "WSA" || x.Command1 == "WSB").Select(x => x.DateTime).FirstOrDefault()
+                , _game.History.Histories.Value.FirstOrDefault(x => x.Command1 == "S1"));
+
+            SetDuration[1] = Duration(_game.History.Histories.Value.Where(x => x.Command1 == "WSA" || x.Command1 == "WSB").Select(x => x.DateTime).Skip(1).FirstOrDefault()
+                , _game.History.Histories.Value.FirstOrDefault(x => x.Command1 == "S2"));
+
+            SetDuration[2] = Duration(_game.History.Histories.Value.Where(x => x.Command1 == "WSA" || x.Command1 == "WSB").Select(x => x.DateTime).Skip(2).FirstOrDefault()
+                , _game.History.Histories.Value.FirstOrDefault(x => x.Command1 == "S3"));
+        }
+        public string ATeamName { get; set; }
+        public string BTeamName { get; set; }
+        public MatchResultSet[] ASet { get; set; } = new MatchResultSet[3];
+        public MatchResultSet[] BSet { get; set; } = new MatchResultSet[3];
+        public MatchResultSet ATotal { get; set; } = new();
+        public MatchResultSet BTotal { get; set; } = new();
+        public TimeSpan?[] SetDuration { get; set; } = new TimeSpan?[3];
+
+        private static TimeSpan? Duration(DateTime dt1, History? history)
+        {
+            if (history is null) return null;
+            if (dt1 == new DateTime()) return null;
+            var dt2 = history.DateTime;
+
+            var d1 = new DateTime(dt1.Year, dt1.Month, dt1.Day, dt1.Hour, dt1.Minute, 0);
+            var d2 = new DateTime(dt2.Year, dt2.Month, dt2.Day, dt2.Hour, dt2.Minute, 0);
+
+            return d1 - d2;
+        }
+        public class MatchResultSet
+        {
+            public int? Timeouts { get; set; }
+            public int? Substitutions { get; set; }
+            public int? Win { get; set; }
+            public int? Points { get; set; }
+        }
+    }
+}
--- VolleyballScoreSheet/ViewModels/ScoreSheet/MatchResultViewModel.cs
+++ VolleyballScoreSheet/ViewModels/ScoreSheet/MatchResultViewModel.cs
@@ -9,72 +9,34 @@ using System.Reactive.Linq;
 using System.Windows.Controls;
 using System.Windows.Controls.Ribbon;
 using Unity.Injection;
-using VolleyballScoreSheet.Model;
+using VolleyballScoreSheet.Model.Scoresheet;
 using VolleyballScoreSheet.Views;
 using Wpf.Ui.Interop.WinDef;
+using static VolleyballScoreSheet.Model.Scoresheet.MatchResult;
 
 namespace VolleyballScoreSheet.ViewModels.ScoreSheet
 {
     public class MatchResultViewModel : BindableBase
     {
-        private readonly Game _game;
         public MatchResultViewModel(Game game)
         {
-            _game = game;
-            ATeamName = _game.ATeam.Value.Name.Value;
-            BTeamName = _game.BTeam.Value.Name.Value;
+            var scoresheet = new MatchResult(game);
 
-            for (int i = 0; i < _game.Set.Value; i++)
-            {
-                ASet[i] = new()
-                {
-                    Timeouts= _game.ATeam.Value.Sets[i].TimeOuts.Value,
-                    Substitutions = _game.ATeam.Value.Sets[i].Substitutions.Value + _game.ATeam.Value.Sets[i].SubstitutionDetails.Where(x => x.ExceptionalSubstitution == true).Count(),
+            ATeamName = scoresheet.ATeamName;
+            BTeamName = scoresheet.BTeamName;
 
-                    Points = _game.ATeam.Value.Sets[i].Points.Value,
-                };
-                BSet[i] = new()
-                {
-                    Timeouts= _game.BTeam.Value.Sets[i].TimeOuts.Value,
-                    Substitutions = _game.BTeam.Value.Sets[i].Substitutions.Value + _game.BTeam.Value.Sets[i].SubstitutionDetails.Where(x => x.ExceptionalSubstitution == true).Count(),
+            ASet = scoresheet.ASet;
+            BSet = scoresheet.BSet;
 
-                    Points = _game.BTeam.Value.Sets[i].Points.Value,
-                };
-
-            }
-
-            SetDuration[0] = TimeSpan(_game.History.Histories.Value.Where(x => x.Command1=="WSA" || x.Command1=="WSB").Select(x => x.DateTime).FirstOrDefault()
-                , _game.History.Histories.Value.FirstOrDefault(x => x.Command1 == "S1"));
-
-            SetDuration[1] = TimeSpan(_game.History.Histories.Value.Where(x => x.Command1=="WSA" || x.Command1=="WSB").Select(x => x.DateTime).Skip(1).FirstOrDefault()
-                , _game.History.Histories.Value.FirstOrDefault(x => x.Command1 == "S2"));
-
-            SetDuration[2] = TimeSpan(_game.History.Histories.Value.Where(x => x.Command1=="WSA" || x.Command1=="WSB").Select(x => x.DateTime).Skip(2).FirstOrDefault()
-                , _game.History.Histories.Value.FirstOrDefault(x => x.Command1 == "S3"));
-
-        }
-        public static TimeSpan? TimeSpan(DateTime dt1, History? hisotry)
-        {
-            if (hisotry is null) return null;
-            var dt2 = hisotry.DateTime;
-
-            var d1 = new DateTime(dt1.Year, dt1.Month, dt1.Day, dt1.Hour, dt1.Minute, 0);
-            var d2 = new DateTime(dt2.Year, dt2.Month, dt2.Day, dt2.Hour, dt2.Minute, 0);
-
-            return d1 - d2;
+            SetDuration = scoresheet.SetDuration;
         }
 
         public string ATeamName { get; set; }
         public string BTeamName { get; set; }
-        public Set[] ASet { get; set; } = new Set[3];
-        public Set[] BSet { get; set; } = new Set[3];
-        public TimeSpan?[] SetDuration { get; set; } = new TimeSpan?[3];
-        public class Set
-        {
-            public int? Timeouts { get; set; }
-            public int? Substitutions { get; set; }
-            public int? Win { get; set; }
-            public int? Points { get; set; }
-        }
+        public MatchResultSet[] ASet { get; set; } = new MatchResultSet[3];
+        public MatchResultSet[] BSet { get; set; } = new MatchResultSet[3];
+        public MatchResultSet ATotal { get; set; } = new();
+        public MatchResultSet BTotal { get; set; } = new();
+        public TimeSpan?[] SetDuration { get; set; }
     }
 }
